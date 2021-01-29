06db3ebd0a7abae12dc4640a12edad6b9027e781
Caleb Brown <caleb@Matthews-MacBook-Pro.local>
2021-01-29 18:05:10 +0000

    On click event

--- src/App.tsx
+++ src/App.tsx
@@ -14,34 +14,44 @@ const Container = styled.div`
 
 const cardData = [
   {
-    id: 1
+    id: 1,
+    name: 'Dog 1',
   },
   {
-    id: 2
+    id: 2,
+    name: 'Dog 2',
   },
   {
-    id: 3
+    id: 3,
+    name: 'Dog 3',
   },
   {
-    id: 4
+    id: 4,
+    name: 'Dog 4',
   },
   {
-    id: 5
+    id: 5,
+    name: 'Dog 5',
   },
   {
-    id: 6
+    id: 6,
+    name: 'Dog 6',
   },
   {
-    id: 7
+    id: 7,
+    name: 'Dog 7',
   },
   {
-    id: 8
+    id: 8,
+    name: 'Dog 8',
   },
   {
-    id: 9
+    id: 9,
+    name: 'Dog 9',
   },
   {
-    id: 10
+    id: 10,
+    name: 'Dog 10',
   }
 ]
 
--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,31 +1,70 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 
 
+const Container = styled.div`
+  position: relative;
+  padding-bottom: 8rem;
+  h1 {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`
+
 const CarouselRow = styled(animated.div)`
   display: flex;
   flex-wrap: nowrap;
+  touch-action: pan-x;
+
+  & > *:not(:first-child) {
+    margin-left: -8rem;
+  }
 `
 
 const StyledCard = styled(animated.div)`
-  width: 300px;
-  height: 300px;
-  margin: 2rem;
+  z-index: ${({ focused }) => focused ? 1 : 0};
+  width: 20rem;
+  height: 20rem;
   background-image: url('https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
   background-size: cover;
   background-position: center;
 `
 
 const Card = (props) => {
+  const [mouseDown, setMouseDown ] = useState(true);
   const { transform } = useSpring({
-    transform: `perspective(700px) rotateY(${props.focused ? 0 : 70}deg) scale(${props.focused ? 1.75 : 1})`,
+    transform: `
+      perspective(700px)
+      rotateY(${props.state === 'prev' ? 70 : props.state === 'next' ? -70 : 0}deg)
+      scale(${props.state === 'current' ? 1 : 0.6})`,
     config: { mass: 5, tension: 500, friction: 80 }
   })
 
+
+  const handleMouseUp = () => {
+    if (mouseDown) {
+      props.onClick();
+    }
+  }
+
+  const handleMouseMove = () => {
+    if (mouseDown) {
+      setMouseDown(false);
+    }
+  }
+
   return (
-    <StyledCard style={{transform: transform.interpolate(t => `${t} rotateY(180deg)`)}} />
+    <StyledCard
+      onMouseDown={() => setMouseDown(true)}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      focused={props.state === 'current'}
+      style={{transform: transform.interpolate(t => `${t} rotateY(180deg)`)}}
+    />
   )
 }
 
@@ -34,20 +73,36 @@ export const Carousel = (props) => {
 
   const carouselRef = useRef(null);
 
-  const [{ x }, set] = useSpring(() => ({ x: 0 }));
+  const [{ x }, set] = useSpring(() => ({ x: -160 }));
+
+  useEffect(() => {
+    const centralCard = carouselRef.current.children[focusedCard];
+    const center = centralCard.offsetLeft + centralCard.offsetWidth / 2;
+
+    return set({
+      x: -center,
+    })
+  }, [focusedCard, set])
 
   // Set the drag hook and define component movement based on gesture data
   const bind = useDrag(({ last, movement: [movementX], memo = x.value}) => {
     if (last) {
-      let newCard = movementX < -50 ? focusedCard + 1 : movementX > 50 ? focusedCard - 1 : focusedCard;
+      let newCard = movementX < -50
+        ? focusedCard + 1
+        : movementX > 50
+        ? focusedCard - 1
+        : focusedCard;
       newCard = newCard < 0 ? 0 : newCard > props.data.length -1 ? props.data.length - 1 : newCard;
-      const centralCard = carouselRef.current.children[newCard];
-      const center = centralCard.offsetLeft;
+      if (newCard !== focusedCard) {
+        return setFocusedCard(newCard);
+      } else {
+        const centralCard = carouselRef.current.children[focusedCard];
+        const center = centralCard.offsetLeft + centralCard.offsetWidth / 2;
 
-      return set({
-        x: -center,
-        onRest: setFocusedCard(newCard)
-      })
+        return set({
+          x: -center,
+        })
+      }
     };
 
     set({
@@ -57,22 +112,25 @@ export const Carousel = (props) => {
     return memo;
   });
 
-  console.log(x)
 
   return (
-    <CarouselRow
-      {...bind()}
-      ref={carouselRef}
-      style={{
-        transform: x.interpolate(x => `translateX(calc(50% + ${x}px))`),
-      }}
-    >
-      {props.data.map((item, i) => 
-        <Card
-          key={item.id}
-          focused={i === focusedCard}
-        />  
-      )}
-    </CarouselRow>
+    <Container>
+      <CarouselRow
+        {...bind()}
+        ref={carouselRef}
+        style={{
+          transform: x.interpolate(x => `translateX(calc(50% + ${x}px))`),
+        }}
+      >
+        {props.data.map((item, i) => 
+          <Card
+            key={item.id}
+            state={i < focusedCard ? 'prev' : i > focusedCard ? 'next' : 'current'}
+            onClick={() => setFocusedCard(i)}
+          />  
+        )}
+      </CarouselRow>
+      <h1>{props.data[focusedCard].name}</h1>
+    </Container>
   )
 }
